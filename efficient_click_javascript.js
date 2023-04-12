835b2df971195f763ce8826d8dd573cd2f1f2007
VipDev-01 <vipul713@gmail.com>
2023-04-12 22:48:00 +0400

    combined keypress and click

--- efficient_click_javascript.js
+++ efficient_click_javascript.js
@@ -1,18 +1,31 @@
 const soundDivs = document.querySelectorAll('.sound');
 
+window.addEventListener('keydown', playSoundKey);
+
 soundDivs.forEach(sound => { 
 
-    sound.addEventListener('click', playSound);
+    sound.addEventListener('click', playSoundClick);
     sound.addEventListener('transitionend', removeClass);
     
 });
 
-function playSound(e) {
+function playSoundClick(e) {
+
     const audio = document.getElementById(this.classList[1]);
     audio.currentTime = 0;
     audio.play();
 
-    const div = document.querySelector('.' + this.classList[1])
+    const div   = document.querySelector('.' + this.classList[1])
+    this.classList.add('playing')
+}
+
+function playSoundKey(e) {
+    const audio = document.getElementById(e.key);
+    if (audio == null) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    const div = document.querySelector('.' + e.key)
     div.classList.add('playing')
 }
 
--- keys_javascript.js
+++ keys_javascript.js
@@ -1,106 +1,22 @@
-const container = document.getElementById('container');
 
-const audioA = document.getElementById('a')
-const audioS = document.getElementById('s')
-const audioD = document.getElementById('d')
-const audioF = document.getElementById('f')
-const audioG = document.getElementById('g')
-const audioH = document.getElementById('h')
-const audioJ = document.getElementById('j')
-const audioK = document.getElementById('k')
-const audioL = document.getElementById('l')
+const soundDivs = document.querySelectorAll('.sound');
 
-const soundA = document.querySelector('.a')
-const soundS = document.querySelector('.s')
-const soundD = document.querySelector('.d')
-const soundF = document.querySelector('.f')
-const soundG = document.querySelector('.g')
-const soundH = document.querySelector('.h')
-const soundJ = document.querySelector('.j')
-const soundK = document.querySelector('.k')
-const soundL = document.querySelector('.l')
+window.addEventListener('keydown', playSound);
 
+soundDivs.forEach (sound => sound.addEventListener('transitionend', removeClass));
 
+function playSound(e) {
+    const audio = document.getElementById(e.key);
+    if (audio == null) return;
+    audio.currentTime = 0;
+    audio.play();
 
-function playsound(e) {
-    const audio = document.querySelector()
+    const div = document.querySelector('.' + e.key)
+    div.classList.add('playing')
 }
 
 
-const delay = 0;
-
-document.addEventListener('keydown', function (event) {
-
-    switch (event.key) {
-
-        case 'a':
-            audioA.currentTime = 0;
-            audioA.play();
-            style(soundA);
-            break;
-
-
-        case 's':
-            audioS.currentTime = 0;
-            audioS.play();
-            style(soundS);
-            break;
-
-        case 'd':
-            audioD.currentTime = 0;
-            audioD.play();
-            style(soundD);
-            break;
-
-        case 'f':
-            audioF.currentTime = 0;
-            audioF.play();
-            style(soundF);
-            break;
-
-        case 'g':
-            audioG.currentTime = 0;
-            audioG.play();
-            style(soundG);
-            break;
-
-        case 'h':
-            audioH.currentTime = 0;
-            audioH.play();
-            style(soundH);
-            break;
-
-        case 'j':
-            audioJ.currentTime = 0;
-            audioJ.play();
-            style(soundJ);
-            break;
-
-        case 'k':
-            audioK.currentTime = 0;
-            audioK.play();
-            style(soundK);
-            break;
-
-        case 'l':
-            audioL.currentTime = 0;
-            audioL.play();
-            style(soundL);
-            break;
-
-    }
-
-});
-
-function style(sound) {
-    sound.style.backgroundColor = 'lightblue'
-
-    setTimeout(() => {
-        sound.style.backgroundColor = 'rgb(116, 167, 94'
-    }, delay);
+function removeClass(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
 }
-
-
-
-
-
